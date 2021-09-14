ba343934bc9375f5973491c3979e7d8ecd501039
大咪 <admin@DESKTOP-330QRIB.com>
2021-09-14 20:56:40 +0800

    add files

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,19 +6,34 @@
         <img src="../assets/logo.png" alt="" />
       </div>
       <!-- 利用element-ui来设置登录form表单 -->
-      <el-form class="login_form">
+      <!-- 1.给表单添加数据绑定 -->
+      <!-- 2.对表单进行验证 -->
+      <!-- 3.添加表单的重置按钮 -->
+      <el-form
+        ref="LoginFormRef"
+        class="login_form"
+        :model="LoginForm"
+        :rules="LoginFormRules"
+      >
         <!--用户名-->
-        <el-form-item>
-          <el-input prefix-icon=" iconfont icon-user"></el-input>
+        <el-form-item prop="username">
+          <el-input
+            prefix-icon=" iconfont icon-user"
+            v-model="LoginForm.username"
+          ></el-input>
         </el-form-item>
         <!--密码-->
-        <el-form-item>
-          <el-input prefix-icon="iconfont icon-3702mima"></el-input>
+        <el-form-item prop="password">
+          <el-input
+            prefix-icon="iconfont icon-3702mima"
+            v-model="LoginForm.password"
+            type="password"
+          ></el-input>
         </el-form-item>
         <!-- 按钮区域 -->
         <el-form-item class="btns">
-          <el-button type="primary">>登录</el-button>
-          <el-button type="info">重置</el-button>
+          <el-button type="primary" @click="Login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -27,7 +42,47 @@
 
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      //这个是登录表单的对象
+      LoginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      //验证规则对象
+      LoginFormRules: {
+        //需要验证用户名
+        username: [
+          //required是必填的项
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        //需要验证密码
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    //ref引用后的实例对象
+    resetLoginForm () {
+      //重置表单
+      this.$refs.LoginFormRef.resetFields()
+    },
+    //表单登录前的预校验
+    Login () {
+      //拿取得还是loginform的实例对象
+      this.$refs.LoginFormRef.validate(async valid => {
+        //判断valid的true或false
+        if (!valid) return;
+        const { data: res } = await this.$http.post('login', this.LoginForm)
+        console.log(res)
+      })
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import './plugins/element.js'
 import './assets/css/global.css'
 //导入字体图标
 import './assets/fonts/iconfont.css'
+//导入axios的包
+import axios from 'axios'
+//请求更路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+//通过vue原型对象进行$http的挂载
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
