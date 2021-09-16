e2792362e0a976cb4b1ffbedf7e9c1eff579fa34
大咪 <admin@DESKTOP-330QRIB.com>
2021-09-17 01:40:18 +0800

    完成了添加用户

--- src/components/User/User.vue
+++ src/components/User/User.vue
@@ -11,16 +11,25 @@
       <!-- 输入框区 -->
       <el-row :gutter="20">
         <!-- input输入框这一行 -->
+        <!-- v-model双向数据绑定queryInfo 的query(关键字才可以搜索) -->
         <el-col :span="9"
-          ><el-input placeholder="请输入内容">
+          ><el-input
+            placeholder="请输入内容"
+            v-model="queryInfo.query"
+            clearable
+            @clear="getUserList"
+          >
             <el-button
+              @click="getUserList"
               slot="append"
               icon="el-icon-search"
             ></el-button> </el-input
         ></el-col>
         <!-- 按钮这一行 -->
         <el-col :span="4">
-          <el-button type="primary">添加</el-button>
+          <el-button type="primary" @click="addDialogVisible = true"
+            >添加用户</el-button
+          >
         </el-col>
       </el-row>
       <!-- table表格区域 -->
@@ -37,7 +46,11 @@
           <template v-slot="scope">
             <!-- 获得这个一行的数据 -->
             <!-- {{ scope.row }} -->
-            <el-switch v-model="scope.row.mg_state"> </el-switch>
+            <el-switch
+              v-model="scope.row.mg_state"
+              @change="userStateChange(scope.row)"
+            >
+            </el-switch>
           </template>
         </el-table-column>
         <el-table-column label="操作">
@@ -83,6 +96,36 @@
       >
       </el-pagination>
     </el-card>
+    <!-- 添加用户区域Dialog -->
+    <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%">
+      <!-- 主体区域 -->
+      <!-- 添加有验证规则的form表单 -->
+      <el-form
+        :model="addForm"
+        :rules="addFormRules"
+        ref="addFormRef"
+        label-width="70px"
+      >
+        <el-form-item label="用户名:" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码:" prop="password">
+          <el-input v-model="addForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱:" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机:" prop="mobile">
+          <el-input v-model="addForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addDialogVisible = false"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -90,6 +133,25 @@
 export default {
   name: 'User',
   data () {
+    //验证邮箱的规则
+    var checkEmail = (rule, value, cb) => {
+      const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
+      //判断
+      if (regEmail.test(value)) {
+        //合法邮箱
+        return cb()
+      }
+      //非法邮箱
+      cb(new Error('请输入合法的邮箱'))
+    }
+    //验证手机号的规则
+    var checkMobile = (rule, value, cb) => {
+      const regMobile = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
+      if (regMobile.test(value)) {
+        return cb()
+      }
+      cb(new Error('请输入合法的手机号'))
+    }
     return {
       //定义一个空数组来接收数据
       userList: [],
@@ -103,7 +165,37 @@ export default {
         pagesize: 2
       },
       //总条数
-      total: 0
+      total: 0,
+      //定义对话框不显示
+      addDialogVisible: false,
+      //定义添加用户的对象
+      addForm: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      //定义验证规则对象
+      addFormRules: {
+        username: [
+          //required必填项
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, message: '请填写邮箱', trigger: 'blur' },
+          //邮箱的验证 用自定义校验规则
+          { validator: checkEmail, trigger: 'blur' }
+        ],
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ]
+      }
     }
   },
   created () {
@@ -128,6 +220,20 @@ export default {
     handleCurrentChange (newPage) {
       this.queryInfo.pagenum = newPage
       this.getUserList()
+    },
+    //监听开关的状态
+    async userStateChange (userInfo) {
+      // console.log(userInfo)
+      //得到一个布尔值
+      //这里的uid是动态数据，我们要模板字符串
+      const { data: res } = await this.$http.put(`users/${userInfo.id}/state/${userInfo.mg_state}`)
+      if (res.meta.status !== 200) {
+        //1.如果失败则把状态还原
+        userInfo.mg_state = !userInfo.mg_state
+        return this.$message.error('更新用户状态失败')
+      }
+      this.$message.success('更新用户状态成功')
+      console.log(res)
     }
   }
 }
